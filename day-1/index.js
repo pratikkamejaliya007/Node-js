94f7a4a94acc6e1b162ad0e96c47adddce733b8b
pratikkamejaliya007 <pratikkamejaliya6157@gmail.com>
2024-07-26 18:33:48 +0530

    File System

--- day-1/index.js
+++ day-1/index.js
@@ -1,5 +1,17 @@
 console.log(__filename)
 
 for(let i=0;i<=5;i++){
-    console.log(i)
+    if(i % 2 == 0){
+        console.log(i)
+    }
 }
+
+let ab=0
+
+const a=setInterval(()=>{
+    console.log(ab++)
+},1000);
+
+setTimeout(()=>{
+    clearInterval(a)
+},5000)
---
+++ day-2/file.js
@@ -0,0 +1,35 @@
+const fs=require("fs")
+
+fs.writeFileSync("hello.txt","intro of html");
+
+fs.writeFileSync("hy.html","basic");
+
+fs.appendFileSync("hello.txt","  this is a basic file");
+
+fs.appendFile("hy.html", "hello.txt", (err) => {
+    if (err) throw err;
+    console.log('Content appended successfully!');
+  });
+
+  fs.unlinkSync("hy.html");
+
+  let a=fs.readFileSync("hello.txt");
+
+  console.log(a.toString())
+
+  fs.renameSync("hello.txt","hy.txt");
+
+  const ff=require("fs/promises")
+
+  const createfile= async ()=>{
+    try{
+        await ff.writeFile("async.txt","intro html")
+
+        await ff.appendFile("async.txt","add file")
+
+    }catch(err){
+        console.log(err)
+    }
+  }
+
+  createfile()
