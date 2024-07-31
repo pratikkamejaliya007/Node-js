467d1cc2bc1c9ba636ee9c0e1e88db4a4de238f1
pratikkamejaliya007 <pratikkamejaliya6157@gmail.com>
2024-07-31 19:27:54 +0530

    Express-Crud

--- express/express.js
+++ express/express.js
@@ -47,6 +47,36 @@ app.get("/deleteData", (req, res) => {
     res.redirect("back");
 });
 
+app.get("/edit",(req,res)=>{
+
+    const id = parseInt(req.query.userid , 10);
+
+    const data=studentData.find(el => el.id == id)
+
+    if(data){
+        res.render("edit",{
+            data : data
+        })
+    }else{
+        res.redirect("/")
+    }
+
+})
+
+app.post("/update", (req, res) => {
+    const id = parseInt(req.query.userid, 10);
+    const { name } = req.body;
+
+    studentData = studentData.map((el) => {
+        if (el.id === id) {
+            return { id, name }; // Update the student with the new name
+        }
+        return el; // Return unchanged student objects
+    });
+
+    res.redirect("/");
+});
+
 app.listen(3000, () => {
     console.log("server start");
 });
